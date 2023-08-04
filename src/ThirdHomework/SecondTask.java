16a4ff9e6d049ff796a4f5fe22751e932fb29679
mkole <margaritakolesnik2003@gmail.com>
2023-08-04 18:44:55 +0300

    commit 3

--- src/ThirdHomework/SecondTask.java
+++ src/ThirdHomework/SecondTask.java
@@ -16,11 +16,11 @@ public class SecondTask {
         }
         if(monthNumber == 1 || monthNumber == 2 || monthNumber == 12) {
             System.out.println("Winter");
-        } else if(monthNumber == 3 || monthNumber == 4 || monthNumber == 5) {
+        } else if(monthNumber >= 3 && monthNumber <= 5) {
             System.out.println("Spring");
-        } else if(monthNumber == 6 || monthNumber == 7 || monthNumber == 8){
+        } else if(monthNumber >= 6 && monthNumber <= 8){
             System.out.println("Summer");
-        } else if(monthNumber == 9 || monthNumber == 10 || monthNumber == 11) {
+        } else if(monthNumber >= 9 && monthNumber <= 11) {
             System.out.println("Autumn");
         } else {
             System.out.println("There is no month with that number");
