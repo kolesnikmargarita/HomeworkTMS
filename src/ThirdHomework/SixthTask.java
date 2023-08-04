da87d1dc4416dee4b2ea7c4fd8ab14397b5e9573
mkole <margaritakolesnik2003@gmail.com>
2023-08-05 01:50:24 +0300

    cycles task 1

--- src/ThirdHomework/SixthTask.java
+++ src/ThirdHomework/SixthTask.java
@@ -2,9 +2,9 @@ package ThirdHomework;
 
 public class SixthTask {
 
-    public static void main(String[] args) {
+    public static void main (String[] args) {
 
-        for(int i = 1; i <= 99; i++) {
+        for (int i = 1; i <= 99; i++) {
             System.out.println(i++);
         }
     }
