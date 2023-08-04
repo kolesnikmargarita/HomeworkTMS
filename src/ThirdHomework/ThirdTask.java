89301e09e83f72afbbd6bf873450b4ffcd28a080
mkole <margaritakolesnik2003@gmail.com>
2023-08-05 01:39:22 +0300

    control structures task 3

--- src/ThirdHomework/ThirdTask.java
+++ src/ThirdHomework/ThirdTask.java
@@ -9,7 +9,7 @@ public class ThirdTask {
         int number;
         Scanner scanner = new Scanner(System.in);
         System.out.print("Enter number: ");
-        if(scanner.hasNextInt()){
+        if(scanner.hasNextInt()) {
             number = scanner.nextInt();
             if(number % 2 == 0) {
                 System.out.println("even number");
