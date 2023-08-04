03e844e7a74c3d684edeeb09dfdd0f9e6126e777
mkole <margaritakolesnik2003@gmail.com>
2023-08-04 19:44:55 +0300

    commit 4

---
+++ src/ThirdHomework/FourthTask.java
@@ -0,0 +1,22 @@
+package ThirdHomework;
+
+import java.util.Scanner;
+
+public class FourthTask {
+    public static void main(String[] args) {
+
+        float t;
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Введите t: ");
+        if(scanner.hasNextFloat()) {
+            t = scanner.nextFloat();
+            if(t > -5f) {
+                System.out.println("Тепло");
+            } else if(t > -20) {
+                System.out.println("Нормально");
+            } else {
+                System.out.println("Холодно");
+            }
+        }
+    }
+}
