f6c1c8fce3be7dacb9663d064820b8dfd611c9c1
mkole <margaritakolesnik2003@gmail.com>
2023-08-04 21:35:07 +0300

    commit 5

---
+++ src/ThirdHomework/FifthTask.java
@@ -0,0 +1,46 @@
+package ThirdHomework;
+
+import java.util.Scanner;
+
+public class FifthTask {
+    public static void main(String[] args) {
+
+        byte color;
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Введите номер цвета радуги: ");
+        while (true) {
+            if(!scanner.hasNextByte()) {
+                String str = scanner.nextLine();
+                System.out.println("Некорректный ввод");
+            } else {
+                break;
+            }
+        }
+        color = scanner.nextByte();
+        switch(color) {
+            case 1:
+                System.out.println("Красный");
+                break;
+            case 2:
+                System.out.println("Оранжевый");
+                break;
+            case 3:
+                System.out.println("Желтый");
+                break;
+            case 4:
+                System.out.println("Зеленый");
+                break;
+            case 5:
+                System.out.println("Голубой");
+                break;
+            case 6:
+                System.out.println("Синий");
+                break;
+            case 7:
+                System.out.println("Фиолетовый");
+                break;
+            default:
+                System.out.println("Цвета с таким номером нет");
+        }
+    }
+}
