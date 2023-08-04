2a9169a8852f8ad0f8f1f8fe1db8a739f750e28a
mkole <margaritakolesnik2003@gmail.com>
2023-08-04 16:45:03 +0300

    commit 1

---
+++ src/main/HelloUser.java
@@ -0,0 +1,13 @@
+package main;
+
+import java.util.Scanner;
+
+public class HelloUser {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter your name: ");
+        String userName = scanner.nextLine();
+        System.out.println("Hello " + userName);
+    }
+}
