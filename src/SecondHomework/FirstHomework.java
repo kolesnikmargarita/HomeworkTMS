c8451c133dd2b7266fc3cff469717403f79c1601
mkole <margaritakolesnik2003@gmail.com>
2023-08-04 21:46:42 +0300

    commit 5

--- src/SecondHomework/FirstHomework.java
+++ src/SecondHomework/FirstHomework.java
@@ -2,7 +2,7 @@ package SecondHomework;
 
 public class FirstHomework {
 
-    public static void main(String[] args) {
+    public static void main1(String[] args) {
         System.out.print("Hello World");
     }
 }
