c8037d7707d98332228ccb79338ae0a2f7dac38f
mkole <margaritakolesnik2003@gmail.com>
2023-08-05 01:52:10 +0300

    cycles task 2

---
+++ src/ThirdHomework/SeventhTask.java
@@ -0,0 +1,11 @@
+package ThirdHomework;
+
+public class SeventhTask {
+
+    public static void main(String[] args) {
+
+        for(int i = 5; i >= 1; i--) {
+            System.out.println(i);
+        }
+    }
+}
