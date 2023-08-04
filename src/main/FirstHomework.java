dd07fe290110d786946ceb956fff6cb72ae4b4b6
mkole <margaritakolesnik2003@gmail.com>
2023-08-04 16:38:37 +0300

    commit 1

--- src/main/FirstHomework.java
+++ src/main/FirstHomework.java
@@ -1,13 +1,8 @@
 package main;
 
-import java.util.Scanner;
-
 public class FirstHomework {
 
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("Enter your name: ");
-        String userName = scanner.nextLine();
-        System.out.println("Hello " + userName);
+        System.out.print("Hello World");
     }
 }
