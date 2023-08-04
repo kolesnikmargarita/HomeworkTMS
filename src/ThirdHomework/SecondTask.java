323c5bcacc9cfecacfafbf4aa73db02d72418329
mkole <margaritakolesnik2003@gmail.com>
2023-08-04 18:41:41 +0300

    commit 2

---
+++ src/ThirdHomework/SecondTask.java
@@ -0,0 +1,29 @@
+package ThirdHomework;
+
+import java.util.Scanner;
+
+public class SecondTask {
+
+    public static void main(String[] args) {
+
+        int monthNumber;
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter number of month: ");
+        if(scanner.hasNextInt()) {
+            monthNumber = scanner.nextInt();
+        } else {
+            monthNumber = 0;
+        }
+        if(monthNumber == 1 || monthNumber == 2 || monthNumber == 12) {
+            System.out.println("Winter");
+        } else if(monthNumber == 3 || monthNumber == 4 || monthNumber == 5) {
+            System.out.println("Spring");
+        } else if(monthNumber == 6 || monthNumber == 7 || monthNumber == 8){
+            System.out.println("Summer");
+        } else if(monthNumber == 9 || monthNumber == 10 || monthNumber == 11) {
+            System.out.println("Autumn");
+        } else {
+            System.out.println("There is no month with that number");
+        }
+    }
+}
