375aa0f7638f528446a5559ec64182ed928f6bd6
mkole <margaritakolesnik2003@gmail.com>
2023-08-05 01:58:31 +0300

    cycles task 4

---
+++ src/ThirdHomework/NinthTask.java
@@ -0,0 +1,13 @@
+package ThirdHomework;
+
+public class NinthTask {
+
+    public static void main(String[] args) {
+
+        int number = 7;
+        while(number <= 98) {
+            System.out.print(number + " ");
+            number += 7;
+        }
+    }
+}
