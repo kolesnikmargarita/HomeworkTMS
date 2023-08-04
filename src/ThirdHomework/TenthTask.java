2d67de75d23e29c8e8acac1eb9d9128de1766568
mkole <margaritakolesnik2003@gmail.com>
2023-08-05 02:00:05 +0300

    cycles task 5

---
+++ src/ThirdHomework/TenthTask.java
@@ -0,0 +1,11 @@
+package ThirdHomework;
+
+public class TenthTask {
+
+    public static void main(String[] args) {
+
+        for(int i = 0; i < 10; i++) {
+            System.out.println(i * -5);
+        }
+    }
+}
