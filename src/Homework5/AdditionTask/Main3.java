33a4cb0e9071a9715f0ced471070533947db48c8
mkole <margaritakolesnik2003@gmail.com>
2023-08-31 17:18:40 +0300

    commit 1

--- src/Homework5/AdditionTask/Main3.java
+++ src/Homework5/AdditionTask/Main3.java
@@ -9,15 +9,15 @@ public class Main3 {
         byte[][] firstArr = {{1, 0, 0, 0}, {0, 1, 0, 0}, {0, 0, 0, 0}};
         byte[][] secondArr = {{1, 2, 3}, {1, 1, 1}, {0, 0, 0}, {2, 1, 0}};
         int[][] resultArr = new int[firstArr.length][firstArr.length];
-        int resultElement = 0;
+        int resultElement;
         for(int i = 0; i < firstArr.length; i++) {
             for(int k = 0; k < secondArr[0].length; k++) {
+                resultElement = 0;
                 for(int j = 0; j < secondArr.length; j++) {
                     resultElement += firstArr[i][j] * secondArr[j][k];
                 }
                 System.out.print(resultElement + " ");
                 resultArr[i][k] = resultElement;
-                resultElement = 0;
             }
         }
         System.out.println("\n" + Arrays.deepToString(resultArr));
